70853a1466b7cd6af55a1e9eb3eb4ec32301f4a8
premja123 <premja@piktorlabs.com>
2022-09-15 11:20:09 +0530

    15/09/2022 11:20AM

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import React, { useState } from 'react';
-import './App.css';
-import MOCK_DATA from './MOCK_DATA.json';
-import {debounce} from 'lodash';
-import Highlighter from 'react-highlight-words';
-
-export default function App() {
-
-const [searchItem, setNewItem]=useState('');
+import Searching from './Searching';
+import React from 'react';
 
 
-const handleText=debounce((text)=>{
-setNewItem(text);
-},1000);
-
-  return (
-    <div className="App">
-
-  <input type="text" className='searchbox'
-   placeholder='Search here' 
-    onChange={
-      (event)=>{handleText(event.target.value);}}
-      />
-
-
-  {MOCK_DATA.filter((val)=>{
-   if(val.name.toLowerCase().includes(searchItem.toLowerCase()))
-        {
-         return val;
-       }
-  }).map((val,key)=>
-
-  {
-    console.log(key);
-    return (
-    <div className="names" key={key}>
-
-      <p>{val.name}</p>
-      
-      </div>
-      );
-  })}
-    </div>
-  )
+ function App() {
+return(
+<Searching/>
+);
 }
+
+export default App;
---
+++ src/Searching.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+import MOCK_DATA from './MOCK_DATA.json';
+import {debounce} from 'lodash';
+// import Highlighter from 'react-highlight-words';
+
+export default function Searching() {
+
+const [searchItem, setNewItem]=useState('');
+
+
+const handleText=debounce((text)=>{
+setNewItem(text);
+},1000);
+
+
+const makeItBold=(texting)=>{
+    return texting.replace(new RegExp(searchItem,"ig"),(match)=>
+match.fontcolor("red").bold()
+    );
+};
+
+  return (
+    <div className="App">
+
+  <input type="text" className='searchbox'
+   placeholder='Search here' 
+    onChange={
+      (event)=>{handleText(event.target.value);}}
+      />
+
+
+  {MOCK_DATA.filter((val)=>{
+   if(val.name.toLowerCase().includes(searchItem.toLowerCase()))
+        {
+         return val;
+       }
+  }).map((val,key)=>
+
+  {
+    console.log(key);
+    return (
+    <div className="names" key={key}>
+
+     {/* <p>{val.name}</p> */}
+     <p dangerouslySetInnerHTML={{
+        __html:makeItBold(val.name),
+     }}/>
+    
+      </div>
+      );
+  })}
+    </div>
+  )
+}
